3d71327ef8dab5549a9d87a402e0671ce24850ad
Huijie Qiao <huijieqiao@gmail.com>
2024-11-09 13:07:55 +0800

    20241109

--- PaleoDB/terrestrial_vertebrates.r
+++ PaleoDB/terrestrial_vertebrates.r
@@ -2,6 +2,7 @@ library(data.table)
 setwd("/media/huijieqiao/WD22T_11/GABI/Script")
 backbone<-fread("../Data/GBIF/backbone/Taxon.tsv", quote="")
 taxa<-readRDS("../Data/PaleoDB/taxa_table.rda")
+taxa<-taxa[!is.na(nam)]
 taxa<-unique(taxa)
 taxa$acc_name<-""
 taxa$syn_name<-""
@@ -11,16 +12,77 @@ taxa$fixed_name<-gsub("  ", " ", taxa$fixed_name)
 taxa[fixed_name=="Katastrophomena (Katastrophomena) woodlandensis"]
 
 backbone[canonicalName=="Spondylus marisrubri"]
-backbone[taxonID==5729648]
+backbone[taxonID==8936276]
+backbone[canonicalName=="Abderospira aldrichi"]
+taxa[syn_name=="Abderospira aldrichi"]
 
-taxa[fixed_name %in% backbone[taxonomicStatus=="accepted"]$canonicalName]$acc_name<-
-  taxa[fixed_name %in% backbone[taxonomicStatus=="accepted"]$canonicalName]$fixed_name
+taxa$match_type<-""
+taxa[fixed_name %in% backbone[taxonomicStatus %in% c("accepted", "doubtful")]$canonicalName]$acc_name<-
+  taxa[fixed_name %in% backbone[taxonomicStatus %in% c("accepted", "doubtful")]$canonicalName]$fixed_name
+taxa[fixed_name %in% backbone[taxonomicStatus=="accepted"]$canonicalName]$match_type<-
+  "ACCEPTED"
+taxa[fixed_name %in% backbone[taxonomicStatus=="doubtful"]$canonicalName]$match_type<-
+  "DOUBTFUL"
 
 taxa[fixed_name %in% backbone[taxonomicStatus!="accepted"]$canonicalName]$syn_name<-
   taxa[fixed_name %in% backbone[taxonomicStatus!="accepted"]$canonicalName]$fixed_name
 
+table(backbone$taxonomicStatus)
+
+table(backbone[is.na(acceptedNameUsageID)]$taxonomicStatus)
+
+table(backbone[!is.na(acceptedNameUsageID)]$taxonomicStatus)
+
+
+backbone_syn<-backbone[taxonomicStatus %in% c("heterotypic synonym",
+                                              "homotypic synonym",
+                                              "proparte synonym ",
+                                              "synonym")]
+backbone_acc<-backbone[taxonomicStatus %in% c("accepted", "doubtful")]
+
+backbone_syn<-backbone_syn[, c("canonicalName", "acceptedNameUsageID", "taxonID",
+                               "taxonomicStatus")]
+backbone_acc<-backbone_acc[, c("canonicalName", "taxonID", "taxonomicStatus")]
+
+backbone_syn_acc<-merge(backbone_syn, backbone_acc, by.x="acceptedNameUsageID", 
+                        by.y="taxonID")
+nrow(backbone_syn_acc)
+nrow(backbone_syn)
+
+
+
+colnames(backbone_syn_acc)<-c("acceptedNameUsageID",
+                              "synonym",
+                              "taxonID",
+                              "synTaxonomicStatus",    
+                              "acceptedName",
+                              "accTaxonomicStatus")
+table(backbone_syn_acc$accTaxonomicStatus)
+backbone_syn[canonicalName=="Abderospira aldrichi"]
+backbone_acc[taxonID=="8936276"]
+backbone[taxonID=="8936276"]
+
+table(taxa$match_type)
+taxa_acc<-merge(taxa, backbone_syn_acc, by.x="fixed_name", by.y="synonym", all.x=T)
+N_taxa_acc<-taxa_acc[, .(N=.N), by="fixed_name"]
+N_taxa_acc[N>1]
+
+taxa_acc[fixed_name=="Acer villosum"]
+
+table(taxa_acc$match_type)
+
+taxa_acc[match_type==""]$match_type<-taxa_acc[match_type==""]$synTaxonomicStatus
+
+taxa_acc[match_type %in% c("heterotypic synonym", "homotypic synonym",
+                           "proparte synonym", "synonym")]$acc_name<-
+  taxa_acc[match_type %in% c("heterotypic synonym", "homotypic synonym",
+                             "proparte synonym", "synonym")]$acceptedName
+
+taxa_acc[is.na(match_type)]$match_type<-""
+table(taxa_acc$match_type)
+
 #https://www.gbif.org/tools/species-lookup
-missing_names<-taxa[acc_name=="" & syn_name==""]
+missing_names<-taxa_acc[acc_name=="" & syn_name==""]
 missing_names<-missing_names[, c("fixed_name")]
 colnames(missing_names)<-"scientificName"
 fwrite(missing_names[1:5000], "../Data/GBIF/missing_names1.csv")
@@ -31,6 +93,8 @@ normalized2<-fread("../Data/GBIF/normalized2.csv")
 normalized<-rbindlist(list(normalized1, normalized2))
 normalized[status!="ACCEPTED" & matchType=="FUZZY"]
 table(normalized$rank)
+table(normalized$status)
+
 normalized<-normalized[, c("verbatimScientificName", "key", "matchType", "status", "rank",
                            "canonicalName", "usageKey", "acceptedUsageKey")]
 normalized[is.na(acceptedUsageKey)]$acceptedUsageKey<-normalized[is.na(acceptedUsageKey)]$usageKey
@@ -39,83 +103,55 @@ normalizedx<-merge(normalized, backbone, by.x="acceptedUsageKey", by.y="taxonID"
 normalizedx<-normalizedx[, c("verbatimScientificName", "matchType",
                              "status", "rank", "canonicalName.x",
                              "canonicalName.y")]
+
 normalizedx<-unique(normalizedx)
 normalizedx$match_type_2<-sprintf("%s_%s", normalizedx$matchType, normalizedx$status)
 table(normalizedx$match_type_2)
 table(normalizedx$status)
 missing_names[!(scientificName %in% normalized$verbatimScientificName)]
+#
+#taxa[syn_name!=""]$match_type<-"Synonym"
+taxa_acc[match_type==""]
 
-taxa<-taxa[!is.na(nam)]
-taxa$match_type<-""
-taxa[acc_name!=""]$match_type<-"Accepted"
-taxa[syn_name!=""]$match_type<-"Synonym"
-taxa[match_type==""]
-
-taxa_with_fuzzy<-merge(taxa, normalizedx, by.x="fixed_name", by.y="verbatimScientificName", all=T)
+taxa_with_fuzzy<-merge(taxa_acc, normalizedx, by.x="fixed_name", 
+                       by.y="verbatimScientificName", all=T)
 taxa_with_fuzzy[match_type==""]$match_type<-taxa_with_fuzzy[match_type==""]$match_type_2
 
 table(taxa_with_fuzzy$match_type)
+taxa_with_fuzzy$match_type<-gsub(" ", "_", toupper(taxa_with_fuzzy$match_type))
+
+
+taxa_with_fuzzy[match_type=="NONE_"]
+
 taxa_with_fuzzy[match_type=="HIGHERRANK_SYNONYM"]
-taxa_with_fuzzy[status=="ACCEPTED"]$acc_name<-
-  taxa_with_fuzzy[status=="ACCEPTED"]$canonicalName.y
-taxa_with_fuzzy[status=="SYNONYM"]$syn_name<-
-  taxa_with_fuzzy[status=="SYNONYM"]$canonicalName.y
+taxa_with_fuzzy[match_type=="FUZZY_DOUBTFUL" & canonicalName.x!=canonicalName.y]
 
+table(taxa_with_fuzzy[acc_name==""]$match_type)
+taxa_with_fuzzy[match_type=="FUZZY_ACCEPTED"]$acc_name<-
+  taxa_with_fuzzy[match_type=="FUZZY_ACCEPTED"]$canonicalName.x
 
+taxa_with_fuzzy[match_type=="FUZZY_DOUBTFUL"]$acc_name<-
+  taxa_with_fuzzy[match_type=="FUZZY_DOUBTFUL"]$canonicalName.x
 
-table(taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "ACCEPTED"]$rank)
-taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "ACCEPTED"]$match_type<-
-  "Accepted_Fuzzy"
-taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "ACCEPTED"]$acc_name<-
-  taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "ACCEPTED"]$canonicalName.x
-taxa_with_fuzzy[match_type=="Accepted_Fuzzy"]
+taxa_with_fuzzy[match_type=="FUZZY_SYNONYM"]$acc_name<-
+  taxa_with_fuzzy[match_type=="FUZZY_SYNONYM"]$canonicalName.y
 
-table(taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "DOUBTFUL"]$status)
+taxa_with_fuzzy[match_type=="HIGHERRANK_ACCEPTED"]$acc_name<-
+  taxa_with_fuzzy[match_type=="HIGHERRANK_ACCEPTED"]$canonicalName.x
 
-taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "ACCEPTED"]$match_type<-
-  "Accepted_Fuzzy"
-taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "ACCEPTED"]$acc_name<-
-  taxa_with_fuzzy[acc_name=="" & matchType=="FUZZY" & status == "ACCEPTED"]$canonicalName.x
-taxa_with_fuzzy[match_type=="Accepted_Fuzzy"]
+taxa_with_fuzzy[match_type=="HIGHERRANK_DOUBTFUL"]$acc_name<-
+  taxa_with_fuzzy[match_type=="HIGHERRANK_DOUBTFUL"]$canonicalName.x
+
+taxa_with_fuzzy[match_type=="HIGHERRANK_SYNONYM"]$acc_name<-
+  taxa_with_fuzzy[match_type=="HIGHERRANK_SYNONYM"]$canonicalName.y
+
 
 
 N_taxa_with_fuzzy<-taxa_with_fuzzy[, .(N=.N), by="fixed_name"]
 
-normalizedx[verbatimScientificName=="Achistrum brevis"]
-taxa[fixed_name=="Achistrum brevis"]
-N_taxa_with_fuzzy[N>1]
-#
+normalizedx[verbatimScientificName=="Cerithium gracile"]
+taxa_with_fuzzy[fixed_name=="Cerithium gracile"]
+N_taxa_with_fuzzy[N==5]
 
+saveRDS(taxa_with_fuzzy, "../Data/PaleoDB/taxa_table_with_fuzzy.rda")
 
-
-no_in_gbif_taxa<-taxa[!nam %in% backbone$canonicalName]
-no_in_gbif_taxa[class=="Mammalia"]
-table(no_in_gbif_taxa[class=="Mammalia"]$order)
-
-table(taxa$phylum)
-table(taxa[phylum=="Chordata"]$class)
-taxa<-taxa[class %in% c("Amphibia", "Aves", "Mammalia", "Reptilia")]
-taxa[is.na(ext)]<-0
-N_ROW1<-taxa[, .(N=.N), by=c("class", "ext")]
-
-taxa_gbif_full<-fread("../Data/GBIF/taxa.gbif.csv")
-taxa_gbif<-taxa_gbif_full[iucnRedListCategory %in% 
-                       c("CR", "DD", "EN", "EX", "LC", "NE", "NT", "VU")]
-taxa_gbif<-taxa_gbif[class %in% c("Amphibia", "Aves", "Mammalia", "Reptilia",
-                                  "Testudines")]
-
-exist_taxa<-taxa[nam %in% taxa_gbif$species]
-
-taxa[class=="Reptilia" & ext==1 & !(nam %in% taxa_gbif$species)]
-backbone[genericName=="Emys" & specificEpithet=="marmorata"]
-backbone[canonicalName=="Emys marmorata"]
-
-#Macrochelys temminckii
-taxa_gbif_full[genus=="Macrochelys"]
-taxa[genus=="Macrochelys"]
-
-
-N_ROW2<-exist_taxa[, .(N_GBIF=.N), by=c("class", "ext")]
-
-
-N_ROW<-merge(N_ROW1, N_ROW2, by=c("class", "ext"), all=T)
