c2f2b9d1c538d16f26a7ae185c62e72613bba29c
Huijie Qiao <huijieqiao@gmail.com>
2026-06-25 09:36:44 +0800

    20260625-morning

--- Figures/net.diversity.r
+++ Figures/net.diversity.r
@@ -12,8 +12,11 @@ if (F){
   sp.with.bridge.SA<-sp.with.bridge[current_continent %in% c("South America", "Two continents")]
   sp.with.bridge.SA$current_continent<-"South America"
   sp.full<-rbindlist(list(sp.with.bridge.NA, sp.with.bridge.SA))
+  year_window_size<-50
+  sp.full$year_window<-floor(sp.full$year/year_window_size)*year_window_size
   N.species<-sp.full[,.(N_SP=length(unique(sp_id))), 
-                            by=list(seed_id, year, NB, DA, current_continent, seed_continent)]
+                            by=list(seed_id, year_window, 
+                                    NB, DA, current_continent, seed_continent)]
   
   N.species$label<-sprintf("%d.%s.%s", N.species$seed_id, N.species$NB, N.species$DA)
   N.species$type<-ifelse(N.species$current_continent==N.species$seed_continent, "Native", "Immigrant")
@@ -27,42 +30,49 @@ if (F){
     print(rrrr)
     seeds<-seeds.all[rep==rrrr]
     item<-N.species[label %in% seeds$label]
-    item.N<-item[,.(N_SP=sum(N_SP), rep=rrrr), by=list(year, NB, DA, seed_continent, type)]
+    item.N<-item[,.(N_SP=sum(N_SP), rep=rrrr), by=list(year_window, NB, DA, seed_continent, type)]
     rep.list[[rrrr]]<-item.N
-    item.N<-item[,.(N_SP=sum(N_SP), rep=rrrr), by=list(year, seed_continent, type)]
+    item.N<-item[,.(N_SP=sum(N_SP), rep=rrrr), by=list(year_window, seed_continent, type)]
     rep.all.list[[rrrr]]<-item.N
   }
   rep.df<-rbindlist(rep.list)
   rep.df.all<-rbindlist(rep.all.list)
   
-  saveRDS(rep.df, "../Data/Tables/N.net.diversity.rep.rda")
-  saveRDS(rep.df.all, "../Data/Tables/N.net.diversity.all.rep.rda")
+  saveRDS(rep.df, sprintf("../Data/Tables/N.net.diversity.rep.window.size.%d.rda", year_window_size))
+  saveRDS(rep.df.all, sprintf("../Data/Tables/N.net.diversity.all.window.size.%d.rep.rda", year_window_size))
   
   
   dt_rep2 <- item
   setorder(dt_rep2, seed_continent, type, year)
   
-  # 2. 计算每一年相较于上一年的差值 (N_SP变化量)
   dt_rep2[, diff := N_SP - shift(N_SP), by = .(seed_continent, type)]
-  
-  # 3. 找出差值最小的那一行（也就是跌幅最深、最异常的点）
   sudden_drop_row <- dt_rep2[which.min(diff)]
-  
-  # 打印结果，你可以看到出问题的具体年份和跌幅 (diff)
   print(sudden_drop_row)
   
 }
+year_window_size<-50
+rep.df<-readRDS(sprintf("../Data/Tables/N.net.diversity.rep.window.size.%d.rda", year_window_size))
+rep.df.all<-readRDS(sprintf("../Data/Tables/N.net.diversity.all.window.size.%d.rep.rda", year_window_size))
 
-rep.df<-readRDS("../Data/Tables/N.net.diversity.rep.rda")
-rep.df.all<-readRDS("../Data/Tables/N.net.diversity.all.rep.rda")
-
-setorder(rep.df.all, seed_continent, type, rep, year)
+setorder(rep.df.all, seed_continent, type, rep, year_window)
 rep.df.all[, net_div_rate := (N_SP-shift(N_SP)) / shift(N_SP), by = .(seed_continent, type, rep)]
-rep.df.all[net_div_rate<=-0.04 & year>-500]
+
+ggplot(rep.df.all[year_window>=-1000])+
+  geom_smooth(aes(x=year_window, y=net_div_rate, color=type, linetype=seed_continent), 
+              method = "loess", span = 0.2, se = FALSE, linewidth = 1.2) + 
+  scale_color_manual(values = c("Native" = "#2166AC", "Immigrant" = "#B2182B")) +
+  labs(x = "Year", y = "Net Diversification Rate", 
+       color = "Type", linetype = "Original continent") +
+  theme_classic() +
+  theme(
+    legend.position = "right",
+    axis.title = element_text(face = "bold")
+  )
+
 
 rep.df.all.mean<-rep.df.all[,.(net_div_rate=mean(net_div_rate, na.rm=T),
                                sd=sd(net_div_rate, na.rm=T)),
-                            by=list(year, seed_continent, type)]
+                            by=list(year_window, seed_continent, type)]
 
-ggplot(rep.df.all.mean[year!=-124])+
-  geom_line(aes(x=year, y=net_div_rate, color=type, linetype=seed_continent))
+ggplot(rep.df.all.mean)+
+  geom_line(aes(x=year_window, y=net_div_rate, color=type, linetype=seed_continent))
